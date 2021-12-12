ae997e981fa4b1c643a8d3d72bde3f77b52cc23e
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2021-12-12 15:08:32 -0600

    Changes at statics info h1 and p

---
+++ app/requests.js
@@ -0,0 +1,10 @@
+export conts request = async (state) => {
+    try {
+        const response = await fetch(`https://api.covidtracking.com/v1/states/${state}/daily.json`);
+        const data = await response.json();
+        return data;
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
