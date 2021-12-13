5f9f75e6b464d6fb5d39dfe2cbbc7672e4b64ffe
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2021-12-13 14:07:54 -0600

    API TOP SOCCER

--- app/requests.js
+++ app/requests.js
@@ -1,10 +1,57 @@
-export conts request = async (state) => {
-    try {
-        const response = await fetch(`https://api.covidtracking.com/v1/states/${state}/daily.json`);
-        const data = await response.json();
-        return data;
-        
-    } catch (error) {
-        console.log(error);
-    }
+export function getData() {
+    fetch("https://api-football-v1.p.rapidapi.com/v3/players/topscorers?league=39&season=2020", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+		"x-rapidapi-key": "685f8c0eaamshe1d63aad603f259p15eac7jsn4890b8464953"
+	}
+})
+    .then(response => {console.log(response);})
+/*     .then(respuesta => respuesta.json())
+    .then(data => {console.log(data);}) */
+ /*    .then(resultado => {topScorers(resultado);}) */
+    .catch(error => console.log("Error en la petición"));
 }
+
+getData();
+
+export function topScorers(objeto) {
+    const h1 = document.querySelector('h3');
+    h1.innerHTML = `Liga: ${objeto.results}`;
+
+}
+
+
+/* 
+const token = 'a611bc23d6bd0a26109572836901c1764cbfb59b4251b6e8800030ad7245a78b';
+
+const url = 'https://apiv2.allsportsapi.com/football/';
+
+const endPoint = `?met=Countries&APIkey=${token}`;
+
+export function getData() {
+
+fetch(`${url}${endPoint}`)
+
+.then(respuesta => respuesta.json())
+
+.then(resultado => {getData2(resultado);})
+
+.catch(error => console.log("Error en la petición"));
+
+}
+
+getData();
+
+export function getData2(objeto) {
+
+const h1 = document.querySelector('h3');
+
+h1.innerHTML = `Liga: ${objeto.result[0].country_name}`;
+
+const img = document.getElementById('logoequipo');
+
+img.src = `${objeto.result[0].country_logo}`;
+
+}
+ */
