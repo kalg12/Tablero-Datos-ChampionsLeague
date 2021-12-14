65f1f3d72aa6bd794e77abb5aef402773deaeae0
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2021-12-13 20:33:38 -0600

    Cambios en fetch

--- app/requests.js
+++ app/requests.js
@@ -1,15 +1,17 @@
+const key = "685f8c0eaamshe1d63aad603f259p15eac7jsn4890b8464953";
+const rapidapi = "api-football-v1.p.rapidapi.com";
+const url = "https://api-football-v1.p.rapidapi.com/v3/";
+
 export function getData() {
-    fetch("https://api-football-v1.p.rapidapi.com/v3/players/topscorers?league=39&season=2020", {
+    fetch(`${url}players/topscorers?league=39&season=2020`, {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-		"x-rapidapi-key": "685f8c0eaamshe1d63aad603f259p15eac7jsn4890b8464953"
+		"x-rapidapi-host": rapidapi,
+		"x-rapidapi-key": key
 	}
 })
-    .then(response => {console.log(response);})
-/*     .then(respuesta => respuesta.json())
-    .then(data => {console.log(data);}) */
- /*    .then(resultado => {topScorers(resultado);}) */
+    .then(response => response.json())
+    .then(data => console.log(data))
     .catch(error => console.log("Error en la petición"));
 }
 
@@ -17,40 +19,28 @@ getData();
 
 export function topScorers(objeto) {
     const h1 = document.querySelector('h3');
-    h1.innerHTML = `Liga: ${objeto.results}`;
+    h1.innerHTML = `Liga: ${objeto.response[0].player.firstname}`;
+    const h2 = document.querySelector('h3');
+    h2.innerHTML = `Liga: ${objeto.response[0].player.name}`;
 
 }
 
 
 /* 
 const token = 'a611bc23d6bd0a26109572836901c1764cbfb59b4251b6e8800030ad7245a78b';
-
 const url = 'https://apiv2.allsportsapi.com/football/';
-
 const endPoint = `?met=Countries&APIkey=${token}`;
-
 export function getData() {
-
 fetch(`${url}${endPoint}`)
-
 .then(respuesta => respuesta.json())
-
 .then(resultado => {getData2(resultado);})
-
 .catch(error => console.log("Error en la petición"));
-
 }
-
 getData();
-
 export function getData2(objeto) {
-
 const h1 = document.querySelector('h3');
-
 h1.innerHTML = `Liga: ${objeto.result[0].country_name}`;
-
 const img = document.getElementById('logoequipo');
-
 img.src = `${objeto.result[0].country_logo}`;
 
 }
